9fd51705f6b56a44c5fea61bd0d9ab4a7f76a476
Saboor-Hamedi <janhamedi49@gmail.com>
2024-01-19 13:46:16 +0700

    initial

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
+  config = function()
+    vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal left toggle<CR>', {})
+    local configs = require("nvim-treesitter.configs")
+    configs.setup({
+        highlight = {enable = true}, 
+        indent = {enable = true},
+        ensure_installed = {"bash",
+        "java",
+        "diff",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "jsonc",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "toml",
+        "php",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",},
+    }) 
+  end
+}
